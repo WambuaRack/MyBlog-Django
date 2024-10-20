1127893e27662c794153dd2f8b58c1e5724b414b
wambuarack <shedrackwambua40@gmail.com>
2024-10-20 12:08:04 +0300

    urls

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404, redirect 
+from .models import Post, blog  
+from django.contrib.auth.decorators import login_required 
+def post_list(request):
+    posts =posts.objects.all().order_by('-created_at')
+    return render(request, 'blog/post_list.html', {'posts': posts})
 # Create your views here.
+@login_required
+def post_create (request):
+    if request.method =='POST':
+        title =request.POST['title']
+        content =request.POST['content']
+        Post.objects.create(title=title, content=content, author=request.user)
+        return redirect('post_list')
+    return render (request,'blog/post_ceate.html')
