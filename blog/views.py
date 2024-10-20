2ac6f612f30f660302761c87fd7c0bd41be08628
wambuarack <shedrackwambua40@gmail.com>
2024-10-20 12:23:10 +0300

    f

--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,18 @@
-from django.shortcuts import render
-from django.shortcuts import render, get_object_or_404, redirect 
-from .models import Post, blog  
-from django.contrib.auth.decorators import login_required 
+# blog/views.py
+
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Post  # Import the Post model only
+from django.contrib.auth.decorators import login_required
+
 def post_list(request):
-    posts =posts.objects.all().order_by('-created_at')
+    posts = Post.objects.all().order_by('-created_at')  # Correctly reference the Post model
     return render(request, 'blog/post_list.html', {'posts': posts})
-# Create your views here.
+
 @login_required
-def post_create (request):
-    if request.method =='POST':
-        title =request.POST['title']
-        content =request.POST['content']
+def post_create(request):
+    if request.method == 'POST':
+        title = request.POST['title']
+        content = request.POST['content']
         Post.objects.create(title=title, content=content, author=request.user)
-        return redirect('post_list')
-    return render (request,'blog/post_ceate.html')
+        return redirect('post_list')  # Redirect to post_list after creating a post
+    return render(request, 'blog/post_create.html')  # Corrected template name
